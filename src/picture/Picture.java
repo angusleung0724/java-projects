6eeedfb1f1d05bb8077b350a8e9f093b3a425343
Angus LEUNG <angus.leung22@imperial.ac.uk>
2023-01-16 16:08:56 +0000

    fixed indents

--- src/picture/Picture.java
+++ src/picture/Picture.java
@@ -11,171 +11,171 @@ import java.io.IOException;
  */
 public class Picture {
 
-    /**
-     * The internal image representation of this picture.
-     */
-    private final BufferedImage image;
+  /**
+   * The internal image representation of this picture.
+   */
+  private final BufferedImage image;
 
-    /**
-     * Construct a new (blank) Picture object with the specified width and height.
-     */
-    public Picture(int width, int height) {
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+  /**
+   * Construct a new (blank) Picture object with the specified width and height.
+   */
+  public Picture(int width, int height) {
+    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+  }
+
+  /**
+   * Construct a new Picture from the image data in the specified file.
+   */
+  public Picture(String filepath) {
+    try {
+      image = ImageIO.read(new File(filepath));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Test if the specified point lies within the boundaries of this picture.
+   *
+   * @param x the x co-ordinate of the point
+   * @param y the y co-ordinate of the point
+   * @return <tt>true</tt> if the point lies within the boundaries of the picture, <tt>false</tt>
+   * otherwise.
+   */
+  public boolean contains(int x, int y) {
+    return x >= 0 && y >= 0 && x < getWidth() && y < getHeight();
+  }
+
+  /**
+   * Returns true if this Picture is graphically identical to the other one.
+   *
+   * @param other The other picture to compare to.
+   * @return true iff this Picture is graphically identical to other.
+   */
+  @Override
+  public boolean equals(Object other) {
+    if (other == null) {
+      return false;
+    }
+    if (!(other instanceof Picture)) {
+      return false;
     }
 
-    /**
-     * Construct a new Picture from the image data in the specified file.
-     */
-    public Picture(String filepath) {
-        try {
-            image = ImageIO.read(new File(filepath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    Picture otherPic = (Picture) other;
+
+    if (image == null || otherPic.image == null) {
+      return image == otherPic.image;
+    }
+    if (image.getWidth() != otherPic.image.getWidth()
+            || image.getHeight() != otherPic.image.getHeight()) {
+      return false;
+    }
+
+    for (int i = 0; i < image.getWidth(); i++) {
+      for (int j = 0; j < image.getHeight(); j++) {
+        if (image.getRGB(i, j) != otherPic.image.getRGB(i, j)) {
+          return false;
         }
+      }
     }
+    return true;
+  }
 
-    /**
-     * Test if the specified point lies within the boundaries of this picture.
-     *
-     * @param x the x co-ordinate of the point
-     * @param y the y co-ordinate of the point
-     * @return <tt>true</tt> if the point lies within the boundaries of the picture, <tt>false</tt>
-     * otherwise.
-     */
-    public boolean contains(int x, int y) {
-        return x >= 0 && y >= 0 && x < getWidth() && y < getHeight();
+  /**
+   * Return the height of the <tt>Picture</tt>.
+   *
+   * @return the height of this <tt>Picture</tt>.
+   */
+  public int getHeight() {
+    return image.getHeight();
+  }
+
+  /**
+   * Return the colour components (red, green, then blue) of the pixel-value located at (x,y).
+   *
+   * @param x x-coordinate of the pixel value to return
+   * @param y y-coordinate of the pixel value to return
+   * @return the RGB components of the pixel-value located at (x,y).
+   * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
+   *                                        the boundaries of this picture.
+   */
+  public Color getPixel(int x, int y) {
+    int rgb = image.getRGB(x, y);
+    return new Color((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff);
+  }
+
+  /**
+   * Return the width of the <tt>Picture</tt>.
+   *
+   * @return the width of this <tt>Picture</tt>.
+   */
+  public int getWidth() {
+    return image.getWidth();
+  }
+
+  @Override
+  public int hashCode() {
+    if (image == null) {
+      return -1;
     }
-
-    /**
-     * Returns true if this Picture is graphically identical to the other one.
-     *
-     * @param other The other picture to compare to.
-     * @return true iff this Picture is graphically identical to other.
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (other == null) {
-            return false;
-        }
-        if (!(other instanceof Picture)) {
-            return false;
-        }
-
-        Picture otherPic = (Picture) other;
-
-        if (image == null || otherPic.image == null) {
-            return image == otherPic.image;
-        }
-        if (image.getWidth() != otherPic.image.getWidth()
-                || image.getHeight() != otherPic.image.getHeight()) {
-            return false;
-        }
-
-        for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight(); j++) {
-                if (image.getRGB(i, j) != otherPic.image.getRGB(i, j)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+    int hashCode = 0;
+    for (int i = 0; i < image.getWidth(); i++) {
+      for (int j = 0; j < image.getHeight(); j++) {
+        hashCode = 31 * hashCode + image.getRGB(i, j);
+      }
     }
+    return hashCode;
+  }
 
-    /**
-     * Return the height of the <tt>Picture</tt>.
-     *
-     * @return the height of this <tt>Picture</tt>.
-     */
-    public int getHeight() {
-        return image.getHeight();
+  public void saveAs(String filepath) {
+    try {
+      ImageIO.write(image, "png", new File(filepath));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    /**
-     * Return the colour components (red, green, then blue) of the pixel-value located at (x,y).
-     *
-     * @param x x-coordinate of the pixel value to return
-     * @param y y-coordinate of the pixel value to return
-     * @return the RGB components of the pixel-value located at (x,y).
-     * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
-     *                                        the boundaries of this picture.
-     */
-    public Color getPixel(int x, int y) {
-        int rgb = image.getRGB(x, y);
-        return new Color((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff);
+  /**
+   * Update the pixel-value at the specified location.
+   *
+   * @param x   the x-coordinate of the pixel to be updated
+   * @param y   the y-coordinate of the pixel to be updated
+   * @param rgb the RGB components of the updated pixel-value
+   * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
+   *                                        the boundaries of this picture.
+   */
+  public void setPixel(int x, int y, Color rgb) {
+
+    image.setRGB(
+            x,
+            y,
+            0xff000000
+                    | (((0xff & rgb.getRed()) << 16)
+                    | ((0xff & rgb.getGreen()) << 8)
+                    | (0xff & rgb.getBlue())));
+  }
+
+  /**
+   * Returns a String representation of the RGB components of the picture.
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    for (int y = 0; y < getHeight(); y++) {
+      for (int x = 0; x < getWidth(); x++) {
+        Color rgb = getPixel(x, y);
+        sb.append("(");
+        sb.append(rgb.getRed());
+        sb.append(",");
+        sb.append(rgb.getGreen());
+        sb.append(",");
+        sb.append(rgb.getBlue());
+        sb.append(")");
+      }
+      sb.append("\n");
     }
-
-    /**
-     * Return the width of the <tt>Picture</tt>.
-     *
-     * @return the width of this <tt>Picture</tt>.
-     */
-    public int getWidth() {
-        return image.getWidth();
-    }
-
-    @Override
-    public int hashCode() {
-        if (image == null) {
-            return -1;
-        }
-        int hashCode = 0;
-        for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight(); j++) {
-                hashCode = 31 * hashCode + image.getRGB(i, j);
-            }
-        }
-        return hashCode;
-    }
-
-    public void saveAs(String filepath) {
-        try {
-            ImageIO.write(image, "png", new File(filepath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Update the pixel-value at the specified location.
-     *
-     * @param x   the x-coordinate of the pixel to be updated
-     * @param y   the y-coordinate of the pixel to be updated
-     * @param rgb the RGB components of the updated pixel-value
-     * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
-     *                                        the boundaries of this picture.
-     */
-    public void setPixel(int x, int y, Color rgb) {
-
-        image.setRGB(
-                x,
-                y,
-                0xff000000
-                        | (((0xff & rgb.getRed()) << 16)
-                        | ((0xff & rgb.getGreen()) << 8)
-                        | (0xff & rgb.getBlue())));
-    }
-
-    /**
-     * Returns a String representation of the RGB components of the picture.
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (int y = 0; y < getHeight(); y++) {
-            for (int x = 0; x < getWidth(); x++) {
-                Color rgb = getPixel(x, y);
-                sb.append("(");
-                sb.append(rgb.getRed());
-                sb.append(",");
-                sb.append(rgb.getGreen());
-                sb.append(",");
-                sb.append(rgb.getBlue());
-                sb.append(")");
-            }
-            sb.append("\n");
-        }
-        sb.append("\n");
-        return sb.toString();
-    }
+    sb.append("\n");
+    return sb.toString();
+  }
 }
